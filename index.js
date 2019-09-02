c5115c4c5b566fdfecb43bf9e8a272122b1939d2
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-02 23:01:25 +0700

    refinement

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
 /*
 GET an array of member which have a length by 3 and contains a string data as Tono, Anton and Budi to each array index respectively
+GET an empty object
+SET count to 0
 
 FOR day = 7 to 31
-    set message as "Tanggal day:"
-    IF day EQUALS TO 7
-        DISPLAY message PlUS all three member's data
-    
-    ELSE IF day MODULO 2
-        DISPLAY message Plus Tono
-    ELSE IF day MODULO 4 
-        DISPLAY message Plus Anton
-    ELSE IF day MODULO 5
-        DISPLAY message Plus Budi
-    ELSE
-        DISPLAY message
+  IF the "Tanggal day" key in object is undefined
+    assign an empty array to the mentioned key
+  END IF
+
+  IF day EQUALS TO 7
+    ASSIGN the value for the object with the reffered key with "Tanggal day" with member data by push method
+  ELSE IF day MODULO 5 EQUALS TO 0
+    redo the assignment for the object with closing message ("Tempat Fitness Tutup")
+  ELSE
+    IF count MODULO 2 EQUALS TO 0
+      PUSH member[0] to the object
     END IF
+    IF count MODULO 4 EQUALS TO 0
+      PUSH member[1] to the object
+    END IF
+    IF count MODULO 5 EQUALS TO 0
+      PUSH member[2] to the object
+    END IF
+ADD count by 1
+END FOR
+DISPLAY object
+    
 */
 
 var member = ['Tono', 'Anton', 'Budi'];
-console.log(`Tanggal 7: ` + member);
+var object = {};
 var count = 0;
-for (var i = 8; i <= 31; i++) {
-  var message = `Tanggal ${i}: `;
-  count++;
-  if (i % 5 === 0) {
-    console.log(message + `Tempat Fitnes Tutup`);
-  } else if (count % 2 === 0 && count % 4 === 0) {
-    console.log(message + member[0], ',', member[1]);
-  } else if (count % 2 === 0) {
-    console.log(message + member[0]);
-  } else if (count % 5 === 0) {
-    console.log(message + member[2]);
-  } else {
-    console.log(message);
-  }
-}
-
-/*
-var member = ['Tono', 'Anton', 'Budi'];
 for (var i = 7; i <= 31; i++) {
-  var message = `Tanggal ${i}: `;
-  var hariIni = i - 7;
-  if (i % 5 === 0) {
-    console.log(message + `Tempat Fitness Tutup`);
-  } else if (hariIni === 2) {
-    console.log(message);
+  if (!object[`Tanggal ${i}`]) {
+    object[`Tanggal ${i}`] = [];
   }
+  if (i === 7) {
+    object[`Tanggal ${i}`].push(member.join(', '));
+    console.log(object);
+  } else if (i % 5 === 0) {
+    object[`Tanggal ${i}`].push(`Tempat Fitness Tutup`);
+  } else {
+    if (count % 2 === 0) {
+      object[`Tanggal ${i}`].push(member[0]);
+    }
+    if (count % 4 === 0) {
+      object[`Tanggal ${i}`].push(member[1]);
+    }
+    if (count % 5 === 0) {
+      object[`Tanggal ${i}`].push(member[2]);
+    }
+  }
+  count++;
+  console.log(object);
 }
+/*
+Kak Nadia, tapi ini utk result nya,
+value si object msh dalam bentuk array,
+aku belum punya ide utk bikin dia tanpa array
+apa hrs pake Object.values kak? tp ga kebayang, kuatir berantakan lagi
+kak Nadia ada hint utk ini?
+thx so much kakkkk
 */
